2f4ad7089c1d55b9b03bd42e21e4b27d84e7b10e
Elizanel <31446686+Elizanel@users.noreply.github.com>
2021-11-07 11:54:08 -0500

    Add files via upload

---
+++ pizzaCost.py
@@ -0,0 +1,13 @@
+D = input("Enter the Diameter: ")
+D = int(D)
+P = input("Enter the prize of pizza: ")
+P = float(P)
+R = D/2
+R = float(R)
+pi= 3.1415
+pi= float(pi)
+Area = pi*R*R
+Area = float(Area)
+C= P/Area
+print("The cost per square inch of a circular pizza is", C) 
+
